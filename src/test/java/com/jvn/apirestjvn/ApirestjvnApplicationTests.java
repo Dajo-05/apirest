cbced7f34f0cc8d16f9baa41709d4f45ca510d89
David Joan Mosquera <daviscell@hotmail.com>
2023-05-17 15:09:19 -0500

    primert commit, configuracion appliaction.property

---
+++ src/test/java/com/jvn/apirestjvn/ApirestjvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvn.apirestjvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestjvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
